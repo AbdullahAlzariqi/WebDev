576add8cbd81ff67557acd09aa4b84d7c1db9bd7
Abdullah Alzariqi <82843876+AbdullahAlzariqi@users.noreply.github.com>
2023-08-08 10:46:45 +0300

    Add files via upload

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,45 +5,28 @@ const ExpressError = require('../utilities/ExpressError');
 const campground = require('../Models/campground');
 const Review = require('../Models/review');
 const { campgroundSchema, reviewSchema } = require('../Schemas/schemas.js');
+const { isLoggedIn, validateReview, isReviewAuthor } = require('../middleware');
 
 
-const validateReview = (req, res, next) => {
-    // Using joi to define  schema for this route 
-    // const campgroundSchema = Joi.object({
-    //     campground: Joi.object({
-    //         title: Joi.string().required(),
-    //         price: Joi.number().required().min(0),
-    //         image: Joi.string().required(),
-    //         description: Joi.string().required()
-    //     }).required()
-    // })
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400)
-    }
-    else {
-        next();
-    }
-}
 
 
 
 
 // adding reviews 
-router.post('/', validateReview, AsyncWrapper(async (req, res) => {
+router.post('/', validateReview, isLoggedIn, AsyncWrapper(async (req, res) => {
     const { id } = req.params;
     const foundCampground = await campground.findById(id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     foundCampground.reviews.push(review);
     await review.save();
     await foundCampground.save();
-    req.flash('success', 'Created a new Review Successfully!');
+    req.flash('success', 'Created a new review successfully!');
     res.redirect(`/campgrounds/${foundCampground._id}`);
 }))
 
 //Deleting a review 
-router.delete('/:reviewId', AsyncWrapper(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, AsyncWrapper(async (req, res) => {
     const { id, reviewId } = req.params;
     await campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(req.params.reviewId);
